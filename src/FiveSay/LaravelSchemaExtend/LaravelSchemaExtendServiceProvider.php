af700af66e10457800283535047472fb3e41ac7e
5-say <bcw.5@foxmail.com>
2014-08-28 11:23:39 +0800

    start

---
+++ src/FiveSay/LaravelSchemaExtend/LaravelSchemaExtendServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace FiveSay\LaravelSchemaExtend;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSchemaExtendServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('five-say/laravel-schema-extend');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
