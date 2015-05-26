517a0d831a7625b09e057a0b5efa480917b6a44a
5-say <bcw.5@foxmail.com>
2015-05-26 23:41:57 +0800

    update

--- src/FiveSay/LaravelSchemaExtend/MySqlGrammar.php
+++ src/FiveSay/LaravelSchemaExtend/MySqlGrammar.php
@@ -7,17 +7,6 @@ use Illuminate\Database\Connection;
 class MySqlGrammar extends \Illuminate\Database\Schema\Grammars\MySqlGrammar
 {
     /**
-     * 初始化
-     * @return void
-     */
-    public function __construct()
-    {
-        if (! in_array('Comment', $this->modifiers)) {
-            array_push($this->modifiers, 'Comment');
-        }
-    }
-
-    /**
      * Get the SQL for a "comment" column modifier.
      *
      * @param \Illuminate\Database\Schema\Blueprint  $blueprint
@@ -43,25 +32,13 @@ class MySqlGrammar extends \Illuminate\Database\Schema\Grammars\MySqlGrammar
      */
     public function compileCreate(Blueprint $blueprint, Fluent $command, Connection $connection)
     {
-        $columns = implode(', ', $this->getColumns($blueprint));
-
-        $sql = 'create table '.$this->wrapTable($blueprint)." ($columns)";
-
-        // Once we have the primary SQL, we can add the encoding option to the SQL for
-        // the table.  Then, we can check if a storage engine has been supplied for
-        // the table. If so, we will add the engine declaration to the SQL query.
-        $sql = $this->compileCreateEncoding($sql, $connection);
+        $sql = parent::compileCreate($blueprint, $command, $connection);
 
         # 表注释支持
         if (isset($blueprint->comment))
         {
             $comment = str_replace("'", "\'", $blueprint->comment);
             $sql .= " comment = '".$comment."'";
-        }
-
-        if (isset($blueprint->engine))
-        {
-            $sql .= ' engine = '.$blueprint->engine;
         }
 
         return $sql;
